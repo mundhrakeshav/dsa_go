e4e20d07fb8454705e09087f6ec89da90f8fe095
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-01-11 11:36:11 +0530

    Fixes

--- stack/stackArray.go
+++ stack/stackArray.go
@@ -1,31 +1,31 @@
 package stack
 
-func createNew[T any](_initialData ...T) []T {
+func CreateNew[T any](_initialData ...T) []T {
 	return []T(_initialData)
 }
 
 // stackPush push to first index of array
-func stackPush[T any](stack []T, v T) []T {
+func StackPush[T any](stack []T, v T) []T {
 	return append([]T{v}, stack...)
 }
 
 // stackLength return length of array
-func stackLength[T any](stack []T) int {
+func StackLength[T any](stack []T) int {
 	return len(stack)
 }
 
 // stackPeak return last input of array
-func  stackPeak[T any](stack []T) any {
+func  StackPeek[T any](stack []T) T {
 	return stack[0]
 }
 
 // stackEmpty check array is empty or not
-func stackEmpty[T any](stack []T) bool {
+func StackEmpty[T any](stack []T) bool {
 	return len(stack) == 0
 }
 
 // stackPop return last input and remove it in array
-func stackPop[T any](stack []T) (T, []T) {
+func StackPop[T any](stack []T) (T, []T) {
 	pop := stack[0]
 	stack = stack[1:]
 	return pop, stack
--- stack/stackArray_test.go
+++ stack/stackArray_test.go
@@ -6,13 +6,13 @@ import (
 
 // TestStackArray for testing Stack with Array
 func TestStackArray(t *testing.T) {
-	var stackArr []int = createNew(5, 1, 2, 43, 5)
+	var stackArr []int = CreateNew(5, 1, 2, 43, 5)
 	if len(stackArr) != cap(stackArr) {
 		t.Error("Len Cap Mismatch")
 	}
 	t.Run("Push", func(t *testing.T) {
 		initialLen := len(stackArr);
-		stackArr = stackPush(stackArr, 23);
+		stackArr = StackPush(stackArr, 23);
 		if len(stackArr) <= initialLen {
 			t.Error("Did not push")
 		}
@@ -21,7 +21,7 @@ func TestStackArray(t *testing.T) {
 	t.Run("Pop", func(t *testing.T) {
 		initialLen := len(stackArr);
 		// var stackArr []int;
-		pop, stackArr := stackPop(stackArr)
+		pop, stackArr := StackPop(stackArr)
 
 		if len(stackArr) >= initialLen {
 			t.Error("Did not pop")
