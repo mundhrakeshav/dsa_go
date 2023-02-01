255e2d19050b103234ecb307975caddc08ba5f67
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-02-01 13:58:43 +0530

    Insertion Sort

--- sort/insertion_sort.go
+++ sort/insertion_sort.go
@@ -2,15 +2,15 @@ package sort
 
 import types "mundhrakeshav/dsa/types"
 
-func Insertion_Sort[T types.Ordered](arr []int) []int {
+func Insertion_Sort[T types.Ordered](arr []T) []T {
 	for i := 1; i < len(arr); i++ {
 		val := arr[i]
-		hole := i
-		for hole > 0 && arr[hole -1] > val {
-			arr[hole] = arr[hole-1]
-			hole = hole - 1
-		}
-		arr[hole] = val;
+			hole := i
+			for hole > 0 &&  arr[hole-1] > val {
+				arr[hole]= arr[hole - 1]
+				hole--;
+			}
+		arr[hole] = val
 	}
 	return arr
 }
