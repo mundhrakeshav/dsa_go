7f316064114c1dca676d5343f1a02bc080d134c9
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-05-05 10:13:53 +0530

    WIP

---
+++ bit_manipulation/swapAdjacentBits.go
@@ -0,0 +1,17 @@
+package bitmanipulation
+
+// Position of rightmost set bit
+// right most set bit mask = x & x``
+
+func SwapAdjBit(num uint8) uint8 {
+	// tempNum := num
+	// for i := 0; tempNum != 0; i++ {
+	// 	x := num << 1
+	// 	y := num >> 1
+	// 	mask_x := (1 << i + 1)
+	// 	mask_y := (1 << i)
+		
+	// }
+	// return num
+	return 1;
+}
