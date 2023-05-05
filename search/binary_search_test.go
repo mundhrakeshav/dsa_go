093c7b60ef34e2d9bb9fb7c6248b45843cd94c90
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-05-05 09:59:22 +0530

    WIP

--- search/binary_search_test.go
+++ search/binary_search_test.go
@@ -19,6 +19,36 @@ func TestSearch(t *testing.T) {
 		if arr[x] != 31 {
 			t.Error("Incorrect Key")
 		}
+
+		arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+		key := 5
+		low := 0
+		high := len(arr) - 1
+		expected := 4
+		result := BinarySearchRecursive(arr, key, low, high)
+		if result != expected {
+			t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+		}
+	
+		// key = 11
+		// low = 0
+		// high = len(arr) - 1
+		// expected = -1
+		// result = BinarySearchRecursive(arr, key, low, high)
+		// if result != expected {
+		// 	t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+		// }
+	
+		// arr = []int{}
+		// key = 5
+		// low = 0
+		// high = len(arr) - 1
+		// expected = -1
+		// result = BinarySearchRecursive(arr, key, low, high)
+		// if result != expected {
+		// 	t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+		// }
+
 		arr = []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
 		x = BinarySearchIterative(arr, 31)
 		if arr[x] != 31 {
@@ -30,6 +60,77 @@ func TestSearch(t *testing.T) {
 		if arr[x] != 63 {
 			t.Error("Incorrect Key")
 		}
+
+    // // Test case where key is at the beginning of the array
+    // arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    // key = 1
+    // low = 0
+    // high = len(arr) - 1
+    // expected = 0
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+
+    // // Test case where key is at the end of the array
+    // arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    // key = 10
+    // low = 0
+    // high = len(arr) - 1
+    // expected = 9
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+
+    // // Test case where key is not in the array
+    // arr = []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+    // key = 11
+    // low = 0
+    // high = len(arr) - 1
+    // expected = -1
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+
+    // // Test case where array has only one element and it is the key
+    // arr = []int{5}
+    // key = 5
+    // low = 0
+    // high = len(arr) - 1
+    // expected = 0
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+
+    // // Test case where array has only one element and it is not the key
+    // arr = []int{5}
+    // key = 6
+    // low = 0
+    // high = len(arr) - 1
+    // expected = -1
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+
+    // // Test case where array is empty
+    // arr = []int{}
+    // key = 5
+    // low = 0
+    // high = len(arr) - 1
+    // expected = -1
+    // result = BinarySearchRecursive(arr, key, low, high)
+    // if result != expected {
+    //     t.Errorf("BinarySearchRecursive(%v, %d, %d, %d) = %d; want %d", arr, key, low, high, result, expected)
+    // }
+	////////////////////////////////!
+
+
+
+
 		arr = []int{1, 2, 9, 20, 31, 45, 63, 70, 100}
 		x = BinarySearchIterative(arr, 63)
 		if arr[x] != 63 {
