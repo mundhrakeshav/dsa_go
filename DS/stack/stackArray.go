644ae7348ef252bc3b231073f10b0d7e2d1307b4
Keshav Mundhra <mundhrakeshav11@gmail.com>
2023-05-08 11:43:37 +0530

    Stack

--- DS/stack/stackArray.go
+++ DS/stack/stackArray.go
@@ -1,32 +1,29 @@
 package stack
 
-func CreateNew[T any](_initialData ...T) []T {
-	return []T(_initialData)
+type Stack[T any] []T
+
+
+func CreateNew[T any](initial_data ...T) Stack[T] {
+	return Stack[T](initial_data);
 }
 
-// stackPush push to first index of array
-func StackPush[T any](stack []T, v T) []T {
-	return append([]T{v}, stack...)
+func (stk Stack[T]) Push(data T) Stack[T] {
+	return append([]T{data}, stk...);
 }
 
-// stackLength return length of array
+func (stk Stack[T]) Pop() Stack[T] {
+	if (len(stk) == 0) {
+		return stk
+	}
+	return stk[1:];
+}
+
+
 func StackLength[T any](stack []T) int {
 	return len(stack)
 }
 
-// stackPeak return last input of array
 func  StackPeek[T any](stack []T) T {
 	return stack[0]
 }
 
-// stackEmpty check array is empty or not
-func StackEmpty[T any](stack []T) bool {
-	return len(stack) == 0
-}
-
-// stackPop return last input and remove it in array
-func StackPop[T any](stack []T) (T, []T) {
-	pop := stack[0]
-	stack = stack[1:]
-	return pop, stack
-}
--- DS/stack/stackArray_test.go
+++ DS/stack/stackArray_test.go
@@ -1,34 +1,171 @@
-package stack
+package stack_test
 
 import (
+	stack "mundhrakeshav/dsa/DS/stack"
+	"reflect"
 	"testing"
 )
 
+type CreateNewTest[T any] struct {
+	input           []T
+	expected_length int
+	name            string
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
 // TestStackArray for testing Stack with Array
 func TestStackArray(t *testing.T) {
-	var stackArr []int = CreateNew(5, 1, 2, 43, 5)
-	if len(stackArr) != cap(stackArr) {
-		t.Error("Len Cap Mismatch")
-	}
+	t.Run("Create New", func(t *testing.T) {
+		createNewTests := []CreateNewTest[int]{
+			{
+				input:           []int{1, 2, 3, 227002894, 68652442, 36672393},
+				expected_length: 6,
+				name:            "Basic Initialization",
+			},
+			{
+				input:           []int{},
+				expected_length: 0,
+				name:            "Empty",
+			},
+			{
+				input:           make([]int, 10000),
+				expected_length: 10000,
+				name:            "Large Data",
+			},
+			{
+				input:           (nil),
+				expected_length: 0,
+				name:            "Large Data",
+			},
+		}
+
+		for _, _tests := range createNewTests {
+			new_stack := stack.CreateNew(_tests.input...)
+			if len(new_stack) != _tests.expected_length {
+				t.Errorf("Expected stack length to be %d, but got %d", _tests.expected_length, len(new_stack))
+			}
+			for i, val := range _tests.input {
+				if !reflect.DeepEqual(new_stack[i], val) {
+					t.Errorf("Expected stack[%d] to be %d, but got %d", i, val, new_stack[i])
+				}
+			}
+		}
+
+		struct_data := []Person{
+			{Name: "Alice", Age: 25},
+			{Name: "Bob", Age: 30},
+		}
+		stack_struct := stack.CreateNew(struct_data...)
+		if len(stack_struct) != len(struct_data) {
+			t.Errorf("Expected stack length to be %d, but got %d", len(struct_data), len(stack_struct))
+		}
+		for i, val := range struct_data {
+			if !reflect.DeepEqual(stack_struct[i], val) {
+				t.Errorf("Expected stack[%d] to be %v, but got %v", i, val, stack_struct[i])
+			}
+		}
+
+	})
+
 	t.Run("Push", func(t *testing.T) {
-		initialLen := len(stackArr);
-		stackArr = StackPush(stackArr, 23);
-		if len(stackArr) <= initialLen {
-			t.Error("Did not push")
+		stk := stack.CreateNew(1, 3, 5, 6, 7, 8, 9, 12, 10, 2, 43)
+		stk_len := len(stk)
+		insrt := 100
+		stk = stk.Push(insrt)
+		if len(stk) != stk_len+1 {
+			t.Errorf("Expected stack length to be %d, but got %d", stk_len+1, len(stk))
+		}
+		if stk[0] != insrt {
+			t.Errorf("Expected 0th element to be %d but got %d", insrt, stk[0])
+		}
+
+		/*------------------------------------------------------------------------------------*/
+
+		// Test case: Pushing to an empty stack
+		empty_stack := stack.CreateNew([]int{}...)
+		empty_stack = empty_stack.Push(1)
+		if len(empty_stack) != 1 {
+			t.Errorf("Expected stack length to be 1, but got %d", len(empty_stack))
+		}
+		if empty_stack[0] != 1 {
+			t.Errorf("Expected stack[0] to be 1, but got %v", empty_stack[0])
+		}
+
+		/*------------------------------------------------------------------------------------*/
+		// Test case: Pushing to a full stack
+		full_stack := stack.CreateNew([]int{1, 2, 3}...)
+		full_stack = full_stack.Push(4)
+		if len(full_stack) != 4 {
+			t.Errorf("Expected stack length to be 4, but got %d", len(full_stack))
+		}
+		/*------------------------------------------------------------------------------------*/
+
+		// Test case: Pushing a large amount of data
+		large_data := make([]int, 10000)
+		stk = stack.CreateNew([]int{1, 2, 3}...)
+		for _, val := range large_data {
+			stk = stk.Push(val)
+		}
+		if len(stk) != 10003 {
+			t.Errorf("Expected stack length to be 1000003, but got %d", len(stk))
+		}
+
+		/*------------------------------------------------------------------------------------*/
+
+		// Test case: Pushing a custom struct
+		type Person struct {
+			Name string
+			Age  int
+		}
+		person := Person{Name: "Alice", Age: 25}
+		struct_stk := stack.CreateNew([]Person{}...)
+		struct_stk = struct_stk.Push(person)
+		if len(struct_stk) != 1 {
+			t.Errorf("Expected stack length to be 1, but got %d", len(struct_stk))
+		}
+		if struct_stk[0] != person {
+			t.Errorf("Expected stack[0] to be %v, but got %v", person, struct_stk[0])
 		}
 
 	})
 	t.Run("Pop", func(t *testing.T) {
-		initialLen := len(stackArr);
-		// var stackArr []int;
-		pop, stackArr := StackPop(stackArr)
+		stk := stack.CreateNew(1, 2, 3, 4, 5)
+		stk = stk.Pop()
+		if len(stk) != 4 {
+			t.Errorf("Expected stack length to be 4, but got %d", len(stk))
+		}
+		if stk[0] != 2 {
+			t.Errorf("Expected top element to be 2, but got %d", stk[0])
+		}
+		/*------------------------------------------------------------------------------------*/
 
-		if len(stackArr) >= initialLen {
-			t.Error("Did not pop")
+		// Pop another element off the stack
+		stk = stk.Pop()
+
+		// Check that the top element was removed
+		if len(stk) != 3 {
+			t.Errorf("Expected stack length to be 3, but got %d", len(stk))
+		}
+		if stk[0] != 3 {
+			t.Errorf("Expected top element to be 3, but got %d", stk[0])
+		}
+		stk = stk.Pop().Pop().Pop()
+
+		// Check that the stack is now empty
+		if len(stk) != 0 {
+			t.Errorf("Expected stack length to be 0, but got %d", len(stk))
 		}
 
-		if pop != 23 {
-			t.Error("Incorrect Pop")
+		// Should not throw if no elements are present
+		stk.Pop().Pop().Pop()
+		
+		// Check that the stack is now empty
+		if len(stk) != 0 {
+			t.Errorf("Expected stack length to be 0, but got %d", len(stk))
 		}
 	})
 }
